2d84a21d878061b5179cbfef5943f4d10461ebf9
abir hal <77001053+AbirHal@users.noreply.github.com>
2021-06-29 16:01:25 +0100

    first test

---
+++ js/newproject1.js
@@ -0,0 +1,11 @@
+/*نتاع الحركة الجميلة ياالهي*/
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
